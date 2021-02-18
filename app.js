d243561cd9c219fdec34c54d7c76e96ee9acb206
jesspsilva <jps936@hotmail.com>
2021-02-18 19:05:51 +0000

    Handle surrender button

--- app.js
+++ app.js
@@ -76,6 +76,9 @@ const app = Vue.createApp({
             this.monsterHealth = 100;
             this.currentRound = 0;
             this.winner = null;
+        },
+        surrender() {
+            this.winner = 'monster';
         }
     }
 });
